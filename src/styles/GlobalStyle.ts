372a1437bd8ff805806a18175ce08cecce5b2972
unknown <thalesf.galvao@gmail.com>
2021-11-22 18:28:28 -0300

    Fix: change styles properties

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,16 +1,19 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+    
     *{
         padding: 0;
         margin: 0;
         box-sizing: border-box;
     }
     html{
-        font-size: 62.2%;
+        font-size: 62.5%;
     }
     body{
         background-color: #282c34;
         font-family: 'Roboto', sans-serif;
     }
-`
+`;
