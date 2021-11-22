fbb8e6ffd4323738f63e0b9ea63bec9866fd3cc0
unknown <thalesf.galvao@gmail.com>
2021-11-22 18:30:44 -0300

    Feat: add ThemeProvider

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Home } from "../pages/Home";
+import { Home } from "../pages/Home/Home";
 
 export const Routes = () => {
-    return(
-        <BrowserRouter>
-            <Switch>
-                <Route path='/' component={Home} exact/>
-            </Switch>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </BrowserRouter>
+  );
+};
