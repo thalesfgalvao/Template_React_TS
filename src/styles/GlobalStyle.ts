3283458e1c6d6e85661126e293e577d6b29aded2
unknown <thalesf.galvao@gmail.com>
2021-11-24 09:16:02 -0300

    Fix: remove imports in Global Styles and add link in index

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,9 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-    @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&display=swap");
-    @import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap");
-    
+
     *{
         padding: 0;
         margin: 0;
